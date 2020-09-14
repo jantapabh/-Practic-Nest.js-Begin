4de7a75423cb1892ccdfcc9608e4ac5c6ba1e654
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-14 22:12:11 +0700

    add commit deleteTodo methods

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,19 +1,25 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { title } from 'process';
 
 @Controller('todo')
 export class TodoController {
 
-    @Get()
+    @Get() //การแสดงค่าหรือดึงค่ามาแสดง
     getTodos() {
         return [1, 2, 3]
     }
 
-    @Post()
+    @Post() //การสร้าง หรือ post การแสดงค่า
 
-        psostTodos(@Body("title") title:string, @Body("subtitle") subtitle:string){
-            console.log(`title: ${title}, subtitle: ${subtitle}`);
-        }
+    psostTodos(@Body("title") title: string, @Body("subtitle") subtitle: string) {
+        console.log(`title: ${title}, subtitle: ${subtitle}`)
+    }
+
+    @Delete("/:id") //การลบค่าตัวแปร
+    deleteTodoById(@Param("id") id: string) {
+        console.log(`id: ${id}`)
+    }
+
 }
 
 
