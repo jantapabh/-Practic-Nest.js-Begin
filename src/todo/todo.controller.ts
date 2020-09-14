6ae27739915d28ceb509642fbe0b161cbbe6c442
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-14 21:24:29 +0700

    add commit

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+
+    @Get()
+    getTodos() {
+        return [1, 2, 3]
+    }
+}
+
+
