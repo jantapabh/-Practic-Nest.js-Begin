4468c8d47b2cfb3f41c69b2a2b19c0426b0ee3c4
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-23 20:50:26 +0700

    add commit

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ import * as cors from 'cors';
 
 async function bootstrap() {
 
+  
   //NestFactory.create จะทำการสร้าง NestJS Application โดยเบื้องต้นจะใช้ Express ในการทำงาน
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
